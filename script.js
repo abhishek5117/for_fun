f420153a9735e8300934f1da26073cb933446c5c
ABHISHEK KUMAR <70750796+abhishek5117@users.noreply.github.com>
2024-01-11 11:20:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 var i = 0;
 var txt1 =
-  "Hi Love.....!  <<               Now I wan't to say something special to you. <<<                So , Please read everything carefully...!                                                                           > When I saw you for the first time < You seems something Special to me.  <<                  As the days goes < you get closer to me....! <<                           I don't know the reason why your thoughts always resonates inside my mind...!                                                     > Everything about you is always intresting for me...!                     << I am somewhat nervous Because I haven't said these words to anyone and I won't say to anyone in future...!                                                     > I Love my Parents so much than anything else in this world....!                    << Now You are the only person  whom I love equally with my parents....!                                                             >I Love U <SweetHeart.....! |                  << Give me One chance to Prove my Love ...!   Abhishek";
+  "Hi Love.....!  <<               Now I wan't to say something special to you. <<<                So , Please read everything carefully...!                                                                           > When I saw you for the first time < You seem something special to me.  <<                  As the days go < you get closer to me....! <<                           I don't know the reason why your thoughts always resonate inside my mind...!                                                     > Everything about you is always interesting for me...!                     << I am somewhat nervous Because I haven't said these words to anyone and I won't say to anyone in future...!                                                     > I Love my Parents so much than anything else in this world....!                    << Now You are the only person  whom I love equally with my parents....!                                                             >I Love U <SweetHeart.....! |                  <<<< Give me One chance to Prove my Love ...!   Abhishek";
 var speed = 55;
 typeWriter();
+
 function typeWriter() {
   if (i < txt1.length) {
     if (txt1.charAt(i) == "<")
-      document.getElementById("text1").innerHTML += "</br>";
+      document.getElementById("text1").innerHTML += "<br>";
     else if (txt1.charAt(i) == ">")
       document.getElementById("text1").innerHTML = "";
     else if (txt1.charAt(i) == "|") {
-      $(".bg_heart").css("background-image", "')");
+      document.querySelector(".bg_heart").style.backgroundImage = "";
     } else document.getElementById("text1").innerHTML += txt1.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
